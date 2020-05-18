f04c627e1c4694e0c1826e69ff4b3c29279e095f
Alex Latham <alexlthm@gmail.com>
2020-05-17 21:38:33 -0600

    Update model relationship, validations, and associated tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,8 +1,6 @@
 class Merchant < ApplicationRecord
+  validates :name, presence: true
+  
   has_many :items
   has_many :invoices
-  # has_many :customers, through: :invoices
-  # has_many :transactions, through: :invoices
-
-  validates :name, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,6 @@
 class Transaction < ApplicationRecord
-  belongs_to :invoice
-  # belongs_to :merchant, through: :invoice
-  # belongs_to :customer, through: :invoice
-
   validates :credit_card_number, presence: true
   validates :result, presence: true
+
+  belongs_to :invoice
 end
