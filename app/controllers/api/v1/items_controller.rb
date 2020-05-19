75ebda86690041c07285fa9326b259ba339f5a3c
Alex Latham <alexlthm@gmail.com>
2020-05-18 20:07:02 -0600

    Remove unnecessary method call `serializable_hash`

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,26 +3,26 @@ module Api
     class ItemsController < ApplicationController
       def show
         item = Item.find(params[:id])
-        render json: ItemSerializer.new(item).serializable_hash
+        render json: ItemSerializer.new(item)
       end
 
       def index
         items = Item.all
-        render json: ItemSerializer.new(items).serializable_hash
+        render json: ItemSerializer.new(items)
       end
 
       def create
         item = Item.new(item_params)
 
         if item.save
-          render json: ItemSerializer.new(item).serializable_hash
+          render json: ItemSerializer.new(item)
         end
       end
 
       def destroy
         item = Item.find(params[:id])
         item.destroy
-        render json: ItemSerializer.new(item).serializable_hash
+        render json: ItemSerializer.new(item)
       end
 
       private
