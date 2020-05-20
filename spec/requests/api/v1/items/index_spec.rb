4c868ee5de5bd2503bf688de4edae27ec3a3161e
Alex Latham <alexlthm@gmail.com>
2020-05-20 15:07:45 -0600

    Consolidate test files

--- spec/requests/api/v1/items/index_spec.rb
+++ spec/requests/api/v1/items/index_spec.rb
@@ -24,4 +24,29 @@ RSpec.describe 'Visitor', type: :request do
     expect(json[:data][1][:attributes][:unit_price]).to  eq(items[1].unit_price)
     expect(json[:data][1][:attributes][:merchant_id]).to eq(items[1].merchant_id)
   end
+
+  it 'can get items for a merchant' do
+    merchant = create(:merchant)
+    items = create_list(:item, 2, merchant: merchant)
+
+    get api_v1_merchant_items_path(merchant)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data].length).to eq(2)
+
+    expect(json[:data][0][:type]).to                     eq('item')
+    expect(json[:data][0][:id]).to                       eq(items[0].id.to_s)
+    expect(json[:data][0][:attributes][:name]).to        eq(items[0].name)
+    expect(json[:data][0][:attributes][:description]).to eq(items[0].description)
+    expect(json[:data][0][:attributes][:unit_price]).to  eq(items[0].unit_price)
+    expect(json[:data][0][:attributes][:merchant_id]).to eq(items[0].merchant_id)
+
+    expect(json[:data][1][:type]).to                     eq('item')
+    expect(json[:data][1][:id]).to                       eq(items[1].id.to_s)
+    expect(json[:data][1][:attributes][:name]).to        eq(items[1].name)
+    expect(json[:data][1][:attributes][:description]).to eq(items[1].description)
+    expect(json[:data][1][:attributes][:unit_price]).to  eq(items[1].unit_price)
+    expect(json[:data][1][:attributes][:merchant_id]).to eq(items[1].merchant_id)
+  end
 end
--- spec/requests/api/v1/merchants/show_spec.rb
+++ spec/requests/api/v1/merchants/show_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'Visitor', type: :request do
     expect(json[:data][:id]).to                eq(merchant.id.to_s)
     expect(json[:data][:attributes][:name]).to eq(merchant.name)
   end
+
+  it 'can get the merchant for an item' do
+    item = create(:item)
+
+    get api_v1_item_merchant_path(item)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:data][:type]).to              eq('merchant')
+    expect(json[:data][:id]).to                eq(item.merchant.id.to_s)
+    expect(json[:data][:attributes][:name]).to eq(item.merchant.name)
+  end
 end
